b4537286841c1764977c3740418a78126f42f9c2
brilliandrea <63876704+brilliandrea@users.noreply.github.com>
2026-01-27 14:08:37 +0000

    Deploying to gh-pages from @ brilliandrea/brilliandrea.github.io@0d3c68c74d5b7b06cf4ddf14e1f30143d5c74609 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "brilliandrea/brilliandrea",
+    "data-repo": "brilliandrea/brilliandrea.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
