bffae3bfd9a444b56ac06660f80ba10564fb2a7f
brilliandrea <63876704+brilliandrea@users.noreply.github.com>
2026-02-04 16:38:38 +0000

    Deploying to gh-pages from @ brilliandrea/brilliandrea.github.io@e4e5f26e1e0e4b54a57e521260ef37a61990b0fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Optimizer implementations",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
